710123e4a358ffde8989f8baa4674151a24f46d8
Ali688273 <iiii688273@gmail.com>
2026-08-13 10:27:52 +0330

    Update MainActivity.java

--- app/src/main/java/com/example/fullrotation/MainActivity.java
+++ app/src/main/java/com/example/fullrotation/MainActivity.java
@@ -1,15 +1,46 @@
-package com.example.fullrotation;
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.example.fullrotation">
 
-import android.os.Bundle;
-import android.content.pm.ActivityInfo;
-import androidx.appcompat.app.AppCompatActivity;
+    <!-- مجوزهای مورد نیاز برای اجرا در پس‌زمینه، پنجره شناور و ری‌ستارت -->
+    <uses-permission android:name="android.permission.SYSTEM_ALERT_WINDOW" />
+    <uses-permission android:name="android.permission.WRITE_SETTINGS" />
+    <uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
+    <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
 
-public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // فعال‌سازی چرخش کاملاً آزاد (شامل ۱۸۰ درجه و برعکس عمودی)
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
-    }
-}
+    <application
+        android:allowBackup="true"
+        android:icon="@drawable/ic_launcher"
+        android:label="چرخش خودکار"
+        android:supportsRtl="true"
+        android:theme="@style/Theme.AppCompat.Light.DarkActionBar">
+
+        <!-- صفحه اصلی برنامه -->
+        <activity
+            android:name=".MainActivity"
+            android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+
+        <!-- سرویس پس‌زمینه برای فعال ماندن چرخش -->
+        <service
+            android:name=".RotationService"
+            android:enabled="true"
+            android:exported="false" />
+
+        <!-- دریافت‌کننده پیام روشن شدن گوشی -->
+        <receiver
+            android:name=".BootReceiver"
+            android:enabled="true"
+            android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.BOOT_COMPLETED" />
+            </intent-filter>
+        </receiver>
+
+    </application>
+
+</manifest>
